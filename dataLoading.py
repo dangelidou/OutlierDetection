435a0d475397d1e610533eeec9fd5f51bfbd5291
Konstantinos Takis <konsttak@csd.auth.gr>
2025-01-03 17:09:44 +0200

    pca(n_comp=2) and scatterplot

--- dataLoading.py
+++ dataLoading.py
@@ -2,6 +2,9 @@ import pandas as pd
 import os
 from scipy.io import arff
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+import matplotlib.pyplot as plt
 
 # # code
 # arff_file = arff.loadarff('PenDigits_withoutdupl_norm_v10.arff')
@@ -16,3 +19,12 @@ df = df.iloc[:, :-1]
 
 kmeans = KMeans(n_clusters=2805, random_state=0).fit(df)
 print(kmeans.score(df))
+
+pca = PCA(n_components=2, svd_solver='auto')
+data_reduced = pca.fit_transform(df)
+
+# Plot the reduced data
+plt.scatter(data_reduced[:, 0], data_reduced[:, 1], c=kmeans.labels_)
+plt.show()
+
+
